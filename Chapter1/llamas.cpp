0134e842d8127ecf0013cdeee5d03ff851a9523c
Antonio Rea <antonioreacifuentes@yahoo.es>
2022-10-11 16:56:10 +0200

    Ejercicio en c++ de CS50

---
+++ Chapter1/llamas.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    int n0, n1, years = 0;    
+    
+    std::cout << "enter the initial numbers of llamas :";
+    std::cin >> n0;
+    std::cout << "enter the final numbers of llamas :";
+    std::cin >> n1;
+    float poblacion = float(n0);    // truncation
+
+    while(poblacion < n1)
+    {
+        poblacion += ((poblacion/3.0) - (poblacion/4.0));  //  poblacion += poblacion/12
+        years++;
+    }
+
+    std::cout << "se necesitan "<< years << " años para llegar alas "<< n1 << " llamas" << std::endl;
+
+    return 0;
+}

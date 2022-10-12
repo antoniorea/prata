33b007e7acbf53185e67ca3e2b0da366e9a8c93d
Antonio Rea <antonioreacifuentes@yahoo.es>
2022-10-12 20:40:04 +0200

    illustra cin

---
+++ Chapter1/getinfo.cc
@@ -0,0 +1,17 @@
+// getinfo.cc -- input and output
+#include <iostream>
+
+int main()
+{
+    // using namespace std;
+
+    int carrots;
+
+    std::cout << "How many carrots do you have?" << std::endl;
+    std::cin >> carrots;                 // C++ input
+    std::cout << "Here are two more. ";
+    carrots = carrots + 2;
+// the next line concatenates output
+    std::cout << "Now you have " << carrots << " carrots." << std::endl;
+    return 0;
+}

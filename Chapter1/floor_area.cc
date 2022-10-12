b904fb41952ef3a2d3e6d3479937fc2a0263ccb8
Antonio Rea <antonioreacifuentes@yahoo.es>
2022-10-12 21:19:55 +0200

    functions 01

--- Chapter1/floor_area.cc
+++ Chapter1/floor_area.cc
@@ -5,15 +5,15 @@
 
 int main()
 {
-    using namespace std;
+    //  using namespace std;
 
     double area;
-    cout << "Enter the floor area, in square feet, of your home: ";
-    cin >> area;
+    std::cout << "Enter the floor area, in square feet, of your home: ";
+    std::cin >> area;
     double side;
     side = sqrt(area);
-    cout << "That's the equivalent of a square " << side
-         << " feet to this side." << endl;
-    cout << "How fascinating!" << endl;
+    std::cout << "That's the equivalent of a square " << side
+         << " feet to this side." << std::endl;
+    std::cout << "How fascinating!" << std::endl;
     return 0;
 }

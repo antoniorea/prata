59c8e4413f925d1af75a2e09d927ad361221677d
Antonio Rea <antonioreacifuentes@yahoo.es>
2022-10-12 20:33:49 +0200

    Archivo que combina C++ y C

---
+++ Chapter1/c_style.cc
@@ -0,0 +1,13 @@
+// myfirst.cpp -- displays a message
+#include <iostream> 
+#include "stdio.h"                        // a PREPROCESSOR directive
+int main()                                      // function header
+{                                               // start of function body
+/* using namespace std;                              make definitions visible */
+std::cout << "Come up and C++ me some time.";         // message
+std::cout << std::endl;                                    // start a new line
+std::cout << "You won't regret it!" << std::endl;          // more output
+
+printf("Hola C\n");
+return 0;                                       // terminate main()
+}                                               // end of function body

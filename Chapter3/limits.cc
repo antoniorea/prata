c7ae7f1bd8937491b23c47ba777fb4462dbef934
Antonio Rea <antonioreacifuentes@yahoo.es>
2022-10-15 10:46:44 +0200

    limits of integers

---
+++ Chapter3/limits.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <climits>
+
+
+int main()
+{
+
+
+int n_int{INT_MAX}; 
+
+
+std::cout << "maximo numero entero : " << n_int << std::endl;
+std::cout << "int number of bytes : " << sizeof n_int << std::endl;
+
+    return 0;
+}

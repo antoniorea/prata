f3203ed9771423a767ece2e76d2d5fffd4b2af4e
Antonio Rea <antonioreacifuentes@yahoo.es>
2022-10-16 12:42:01 +0200

    the char type and integer type contrasted

---
+++ Chapter3/morechar.cc
@@ -0,0 +1,12 @@
+// morechar.cC -- the char type and integer type contrasted
+#include <iostream>
+
+
+int main()
+{   
+
+    for( char a{'a'}, A{'A'}; a <= 'z' ; a++,A++)    
+            std::cout << "The ASCII code for " << a << " is " << short(a) << '\t'            
+            << "and for " << A << " is " << short(A) << std::endl;   
+    return 0;
+}

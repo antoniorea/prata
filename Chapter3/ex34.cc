15949fa0caf28ddd6e8a6acfdcdf737ac69f7674
Antonio Rea <antonioreacifuentes@yahoo.es>
2022-10-20 18:22:15 +0200

    seconds to days

---
+++ Chapter3/ex34.cc
@@ -0,0 +1,28 @@
+// ex34.cc -- small arrays of integers cstyle
+#include <iostream>
+#include <cstdio> 
+
+int main ()
+{
+    unsigned short const SECONDS_MINUTE {60};
+    unsigned short const MINUTES_HOUR {60};
+    unsigned short const HOURS_DAY {24};
+    unsigned long seconds{}, minutes{}, hours{};
+    unsigned short minutos{}, horas{}, dias{}, segundos{};
+
+    std::cout << "Enter the number of seconds: ";
+    std::cin >> seconds;
+
+    segundos = seconds % SECONDS_MINUTE;
+    minutes = seconds / SECONDS_MINUTE;
+    
+    minutos = minutes % MINUTES_HOUR;
+    hours = minutes / MINUTES_HOUR;
+ 
+    horas = hours % HOURS_DAY;
+    dias = hours / HOURS_DAY;
+
+    printf("%d seconds = %d days,%d hours,%d minutes,%d seconds\n", 
+        seconds, dias, horas, minutos, segundos);
+    return 0;
+}
